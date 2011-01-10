5ab65d5b00cde2d6724ad6bffd5870696d99a152
Jesse Wolfe <jes5199@gmail.com>
2011-01-09 23:51:32 -0800

    when to allow parameters

--- schema_schema.js
+++ schema_schema.js
@@ -6,8 +6,29 @@
     "values": ["string", "number", "boolean", "null", "undefined", "array", "object", "anything", "integer", "enum", "range", "tuple", "dictionary", "either", "restrict" ]
   }],
 
-  "definition_name" : "string",
-  "definition_pair" : ["tuple", {"elements": ["builtin_type", "definition_parameters"]}],
-  "definition_parameters" : "dictionary"
+  "definition_name" : [ "either", {
+    "choices": [ "custom_type", "builtin_type_with_optional_parameters", "builtin_type_without_parameters" ]
+  } ],
 
+  "parameterized_type" : ["either", {"choices": ["builtin_type_with_optional_parameters", "builtin_type_with_mandatory_parameters"] }],
+  "definition_pair" : ["tuple", {"elements": ["parameterized_type", "definition_parameters"]}],
+
+  "definition_parameters" : "dictionary",
+
+  "custom_type" : ["restrict", {
+    "require": ["string"],
+    "reject":  ["builtin_type"]
+  }],
+
+  "builtin_type_without_parameters" : ["enum", {
+    "values": ["string", "number", "boolean", "null", "undefined", "anything", "integer"]
+  }],
+
+  "builtin_type_with_optional_parameters" :  ["enum", {
+    "values": ["array", "object", "dictionary", "restrict"]
+  }],
+
+  "builtin_type_with_mandatory_parameters" : ["enum", {
+    "values": ["enum", "range", "tuple", "either"]
+  }]
 }

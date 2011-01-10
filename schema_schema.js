47ca121a08635987e7657af111f0e21f00b433c5
Jesse Wolfe <jes5199@gmail.com>
2011-01-09 23:30:47 -0800

    only builtins could have parameters

--- schema_schema.js
+++ schema_schema.js
@@ -1,7 +1,13 @@
 {
   "schema" : ["dictionary", {"contents": "definitions"}],
   "definitions" : ["either", {"choices": ["definition_name", "definition_pair"]}],
+
+  "builtin_type" : ["enum", {
+    "values": ["string", "number", "boolean", "null", "undefined", "array", "object", "anything", "integer", "enum", "range", "tuple", "dictionary", "either", "restrict" ]
+  }],
+
   "definition_name" : "string",
-  "definition_pair" : ["tuple", {"elements": ["definition_name", "definition_parameters"]}],
+  "definition_pair" : ["tuple", {"elements": ["builtin_type", "definition_parameters"]}],
   "definition_parameters" : "dictionary"
+
 }

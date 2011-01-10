08a9405d489675527e9353768a0546e123e3a6ba
Jesse Wolfe <jes5199@gmail.com>
2011-01-09 23:24:48 -0800

    fairly naive dogfood

--- schema_schema.js
+++ schema_schema.js
@@ -1,4 +1,7 @@
 {
   "schema" : ["dictionary", {"contents": "definitions"}],
-  "definitions" : "anything"
+  "definitions" : ["either", {"choices": ["definition_name", "definition_pair"]}],
+  "definition_name" : "string",
+  "definition_pair" : ["tuple", {"elements": ["definition_name", "definition_parameters"]}],
+  "definition_parameters" : "dictionary"
 }

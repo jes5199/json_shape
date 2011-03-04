a4a2946b68b31635e9bfccba6d308f411b02f793
Jesse Wolfe <jes5199@gmail.com>
2011-03-03 16:01:41 -0800

    wip: hm! my tests still pass

--- schema_check_spec.rb
+++ schema_check_spec.rb
@@ -1,213 +1,213 @@
-require 'schema_check'
+require 'json_shape'
 
-describe "schema_check" do
+describe "JsonShape.schema_check" do
   describe "the anything type" do
     it "should validate strings" do
-      schema_check( "x", "anything" )
+      JsonShape.schema_check( "x", "anything" )
     end
   end
 
   describe "the literal type" do
     it "should match literal matches" do
-      schema_check( "x", ["literal", "x"] )
-      schema_check( {"x"=>"y"}, ["literal", {"x"=>"y"}] )
+      JsonShape.schema_check( "x", ["literal", "x"] )
+      JsonShape.schema_check( {"x"=>"y"}, ["literal", {"x"=>"y"}] )
     end
 
     it "should not match if the literal does not match" do
-      lambda { schema_check( "x", ["literal", {"x" => "y"}] ) }.should raise_error
-      lambda { schema_check( {"x"=>"y"}, ["literal", {"x"=>"z"}] ) }.should raise_error
-      lambda { schema_check( 1, ["literal", "1"] ) }.should raise_error
+      lambda { JsonShape.schema_check( "x", ["literal", {"x" => "y"}] ) }.should raise_error
+      lambda { JsonShape.schema_check( {"x"=>"y"}, ["literal", {"x"=>"z"}] ) }.should raise_error
+      lambda { JsonShape.schema_check( 1, ["literal", "1"] ) }.should raise_error
     end
   end
 
-  describe "the 'string' type" do 
+  describe "the 'string' type" do
     it "should validate strings" do
-      schema_check( "x", "string" )
+      JsonShape.schema_check( "x", "string" )
     end
     it "should reject numbers" do
-      lambda { schema_check( 1, "string" ) }.should raise_error
+      lambda { JsonShape.schema_check( 1, "string" ) }.should raise_error
     end
     it "should reject objects" do
-      lambda { schema_check( {}, "string" ) }.should raise_error
+      lambda { JsonShape.schema_check( {}, "string" ) }.should raise_error
     end
     it "should reject null" do
-      lambda { schema_check( nil, "string" ) }.should raise_error
+      lambda { JsonShape.schema_check( nil, "string" ) }.should raise_error
     end
     it "should reject arrays" do
-      lambda { schema_check( ["a"], "string" ) }.should raise_error
+      lambda { JsonShape.schema_check( ["a"], "string" ) }.should raise_error
     end
     it "should reject bools" do
-      lambda { schema_check( true, "string" ) }.should raise_error
-      lambda { schema_check( false, "string" ) }.should raise_error
+      lambda { JsonShape.schema_check( true, "string" ) }.should raise_error
+      lambda { JsonShape.schema_check( false, "string" ) }.should raise_error
     end
 
     describe "with parameters" do
       it "should validate strings" do
-        schema_check( "x", ["string", {}] )
+        JsonShape.schema_check( "x", ["string", {}] )
       end
     end
   end
 
   describe "the range predicate" do
     it "should reject strings" do
-      lambda { schema_check( "1", ["range", {}] ) }.should raise_error
+      lambda { JsonShape.schema_check( "1", ["range", {}] ) }.should raise_error
     end
 
     it "should accept numbers in the range" do
-      schema_check(   1, ["range", {"limits" => [0,3]}] )
-      schema_check(   0, ["range", {"limits" => [0,3]}] )
-      schema_check(   3, ["range", {"limits" => [0,3]}] )
-      schema_check( 2.8, ["range", {"limits" => [0,3]}] )
-      schema_check( 3.0, ["range", {"limits" => [0,3]}] )
+      JsonShape.schema_check(   1, ["range", {"limits" => [0,3]}] )
+      JsonShape.schema_check(   0, ["range", {"limits" => [0,3]}] )
+      JsonShape.schema_check(   3, ["range", {"limits" => [0,3]}] )
+      JsonShape.schema_check( 2.8, ["range", {"limits" => [0,3]}] )
+      JsonShape.schema_check( 3.0, ["range", {"limits" => [0,3]}] )
     end
 
     it "should reject numbers out of the range" do
-      lambda { schema_check(  -1, ["range", {"limits" => [0,3]}] ) }.should raise_error
-      lambda { schema_check(   4, ["range", {"limits" => [0,3]}] ) }.should raise_error
-      lambda { schema_check( 3.1, ["range", {"limits" => [0,3]}] ) }.should raise_error
+      lambda { JsonShape.schema_check(  -1, ["range", {"limits" => [0,3]}] ) }.should raise_error
+      lambda { JsonShape.schema_check(   4, ["range", {"limits" => [0,3]}] ) }.should raise_error
+      lambda { JsonShape.schema_check( 3.1, ["range", {"limits" => [0,3]}] ) }.should raise_error
     end
   end
 
   describe "the array type" do
     it "should accept arrays" do
-      schema_check(  [1], "array" )
+      JsonShape.schema_check(  [1], "array" )
     end
     it "should accept arrays of the right type" do
-      schema_check(  [1], ["array", {"contents" => "number"}] )
+      JsonShape.schema_check(  [1], ["array", {"contents" => "number"}] )
     end
     it "should reject arrays of the wrong type" do
-      lambda { schema_check(  [[]], ["array", {"contents" => "number"}] ) }.should raise_error
+      lambda { JsonShape.schema_check(  [[]], ["array", {"contents" => "number"}] ) }.should raise_error
     end
   end
 
   describe "the either type" do
     it "should accept any one of the given subtypes" do
-      schema_check( [], ["either", {"choices" => ["array", "number"]}] )
-      schema_check(  1, ["either", {"choices" => ["array", "number"]}] )
+      JsonShape.schema_check( [], ["either", {"choices" => ["array", "number"]}] )
+      JsonShape.schema_check(  1, ["either", {"choices" => ["array", "number"]}] )
     end
 
     it "should reject an unlisted subtype" do
-      lambda{ schema_check(  false, ["either", {"choices" => ["array", "number"]}] ) }.should raise_error
+      lambda{ JsonShape.schema_check(  false, ["either", {"choices" => ["array", "number"]}] ) }.should raise_error
     end
   end
 
   describe "the enum type" do
     it "should accept any of the given values" do
-      schema_check(    "hello", ["enum", {"values" => ["hello", "goodbye"]}] )
-      schema_check(  "goodbye", ["enum", {"values" => ["hello", "goodbye"]}] )
+      JsonShape.schema_check(    "hello", ["enum", {"values" => ["hello", "goodbye"]}] )
+      JsonShape.schema_check(  "goodbye", ["enum", {"values" => ["hello", "goodbye"]}] )
     end
     it "should reject any other value" do
-      lambda { schema_check(    "elephant", ["enum", {"values" => ["hello", "goodbye"]}] ) }.should raise_error
-      lambda { schema_check(            {}, ["enum", {"values" => ["hello", "goodbye"]}] ) }.should raise_error
+      lambda { JsonShape.schema_check(    "elephant", ["enum", {"values" => ["hello", "goodbye"]}] ) }.should raise_error
+      lambda { JsonShape.schema_check(            {}, ["enum", {"values" => ["hello", "goodbye"]}] ) }.should raise_error
     end
   end
 
   describe "the tuple type" do
     it "should accept an array of the given types" do
-      schema_check( ["a", 1, [2]], ["tuple", {"elements" => ["string", ["range", {"limits" => [0,1]}], ["array", {"contents" => "number" }]  ]}] )
+      JsonShape.schema_check( ["a", 1, [2]], ["tuple", {"elements" => ["string", ["range", {"limits" => [0,1]}], ["array", {"contents" => "number" }]  ]}] )
     end
 
     it "should not accept anything that isn't an array" do
       lambda {
-        schema_check( {}, ["tuple", {"elements" => ["string", ["range", {"limits" => [0,1]}], ["array", {"contents" => "number" }]  ]}] )
+        JsonShape.schema_check( {}, ["tuple", {"elements" => ["string", ["range", {"limits" => [0,1]}], ["array", {"contents" => "number" }]  ]}] )
       }.should raise_error
     end
     it "should not accept an array that is too short" do
       lambda {
-        schema_check( ["a", 1], ["tuple", {"elements" => ["string", ["range", {"limits" => [0,1]}], ["array", {"contents" => "number" }]  ]}] )
+        JsonShape.schema_check( ["a", 1], ["tuple", {"elements" => ["string", ["range", {"limits" => [0,1]}], ["array", {"contents" => "number" }]  ]}] )
       }.should raise_error
     end
     it "should not accept an array that is too long" do
       lambda {
-        schema_check( ["a", 1, [2], 5], ["tuple", {"elements" => ["string", ["range", {"limits" => [0,1]}], ["array", {"contents" => "number" }]  ]}] )
+        JsonShape.schema_check( ["a", 1, [2], 5], ["tuple", {"elements" => ["string", ["range", {"limits" => [0,1]}], ["array", {"contents" => "number" }]  ]}] )
       }.should raise_error
     end
     it "should not accept an array where an entry has the wrong type" do
       lambda {
-        schema_check( ["a", 1, ["b"]], ["tuple", {"elements" => ["string", ["range", {"limits" => [0,1]}], ["array", {"contents" => "number" }]  ]}] )
+        JsonShape.schema_check( ["a", 1, ["b"]], ["tuple", {"elements" => ["string", ["range", {"limits" => [0,1]}], ["array", {"contents" => "number" }]  ]}] )
       }.should raise_error
     end
   end
   describe "the integer type" do
     it "should accept integers" do
-      schema_check( 1, "integer" )
+      JsonShape.schema_check( 1, "integer" )
     end
     it "should reject floats" do
-      lambda{ schema_check( 1.0, "integer" ) }.should raise_error
+      lambda{ JsonShape.schema_check( 1.0, "integer" ) }.should raise_error
     end
     it "should reject strings" do
-      lambda{ schema_check( "1", "integer" ) }.should raise_error
+      lambda{ JsonShape.schema_check( "1", "integer" ) }.should raise_error
     end
   end
 
   describe "the object type" do
     it "should accept an object" do
-      schema_check( {}, "object" )
+      JsonShape.schema_check( {}, "object" )
     end
 
     it "should accept an object with the correct members" do
-      schema_check( {"a" => 1}, ["object", {"members" => {"a" => "integer" } } ] )
+      JsonShape.schema_check( {"a" => 1}, ["object", {"members" => {"a" => "integer" } } ] )
     end
 
     it "should reject an object with missing members" do
-      lambda { schema_check( {"a" => 1}, ["object", {"members" => {"a" => "integer", "b" => "integer" } } ] ) }.should raise_error
+      lambda { JsonShape.schema_check( {"a" => 1}, ["object", {"members" => {"a" => "integer", "b" => "integer" } } ] ) }.should raise_error
     end
 
     it "should reject an object with incorrect members" do
-      lambda { schema_check( {"a" => 1}, ["object", {"members" => {"a" => "string" } } ] ) }.should raise_error
+      lambda { JsonShape.schema_check( {"a" => 1}, ["object", {"members" => {"a" => "string" } } ] ) }.should raise_error
     end
 
     it "should accept an object with missing members if they are of type undefined" do
-      schema_check( {"a" => 1}, ["object", {"members" => {"a" => "integer", "b" => "undefined" } } ] )
+      JsonShape.schema_check( {"a" => 1}, ["object", {"members" => {"a" => "integer", "b" => "undefined" } } ] )
     end
 
     it "should reject an object with extra members" do
-      lambda { schema_check( {"a" => 1, "b" => 2}, ["object", {"members" => {"a" => "integer" } } ] ) }.should raise_error
+      lambda { JsonShape.schema_check( {"a" => 1, "b" => 2}, ["object", {"members" => {"a" => "integer" } } ] ) }.should raise_error
     end
   end
 
   describe "the dictionary type" do
     it "should accept an object" do
-      schema_check( {}, "dictionary" )
+      JsonShape.schema_check( {}, "dictionary" )
     end
 
     it "should accept values of the correct type" do
-      schema_check(  {"a" => 1}, ["dictionary", {"contents" => "number"}] )
+      JsonShape.schema_check(  {"a" => 1}, ["dictionary", {"contents" => "number"}] )
     end
 
     it "should reject values of the wrong type" do
-      lambda { schema_check(  {"a" => []}, ["dictionary", {"contents" => "number"}] ) }.should raise_error
+      lambda { JsonShape.schema_check(  {"a" => []}, ["dictionary", {"contents" => "number"}] ) }.should raise_error
     end
 
     it "should respect custom types" do
-      schema_check(  {"a" => 1}, ["dictionary", {"contents" => "foo"}], {"foo" => "number"} )
+      JsonShape.schema_check(  {"a" => 1}, ["dictionary", {"contents" => "foo"}], {"foo" => "number"} )
     end
   end
 
   describe "the boolean type" do
     it "should accept true" do
-      schema_check( true, "boolean" )
+      JsonShape.schema_check( true, "boolean" )
     end
     it "should accept false" do
-      schema_check( false, "boolean" )
+      JsonShape.schema_check( false, "boolean" )
     end
     it "should reject other values" do
-      lambda{ schema_check( 1, "boolean" ) }.should raise_error
+      lambda{ JsonShape.schema_check( 1, "boolean" ) }.should raise_error
     end
   end
 
   describe "the null type" do
     it "should accept null" do
-      schema_check( nil, "null" )
+      JsonShape.schema_check( nil, "null" )
     end
     it "should reject other values" do
-      lambda{ schema_check( 1, "null" ) }.should raise_error
+      lambda{ JsonShape.schema_check( 1, "null" ) }.should raise_error
     end
   end
 
   describe "the restrict type" do
     it "should accept a value that satisfies multiple requirements" do
-      schema_check( 2,
+      JsonShape.schema_check( 2,
         ["restrict",
           {
             "require" => [
@@ -222,7 +222,7 @@ describe "schema_check" do
     end
     it "should reject a value that fails to satisfy a requirement" do
       lambda {
-        schema_check( 2,
+        JsonShape.schema_check( 2,
           ["restrict",
             {
               "require" => [
@@ -238,7 +238,7 @@ describe "schema_check" do
     end
     it "should reject a value that satisfies a rejection constraint" do
       lambda {
-        schema_check( 2,
+        JsonShape.schema_check( 2,
           ["restrict",
             {
               "reject" => [
@@ -252,7 +252,7 @@ describe "schema_check" do
     end
 
     it "should accept a value that satisfies requirements and avoids rejections" do
-      schema_check( 2,
+      JsonShape.schema_check( 2,
         ["restrict",
           {
             "require" => [
@@ -272,7 +272,7 @@ describe "schema_check" do
 
     it "should reject a value that satisfies requirements but violates a rejection rule" do
       lambda {
-        schema_check( 2,
+        JsonShape.schema_check( 2,
           ["restrict",
             {
               "require" => [
@@ -293,7 +293,7 @@ describe "schema_check" do
 
     it "should reject a value that fails to satisfy requirements but doesn't violate a rejection rule" do
       lambda {
-        schema_check( 2,
+        JsonShape.schema_check( 2,
           ["restrict",
             {
               "require" => [
@@ -316,20 +316,20 @@ describe "schema_check" do
 
   describe "named types" do
     it "should work" do
-      schema_check( 2, "foo", { "foo" => "integer" } )
+      JsonShape.schema_check( 2, "foo", { "foo" => "integer" } )
     end
 
     it "should enforce the refered type" do
-      lambda { schema_check( 2, "foo", { "foo" => "array" } ) }.should raise_error
+      lambda { JsonShape.schema_check( 2, "foo", { "foo" => "array" } ) }.should raise_error
     end
 
     it "should work recursively" do
-      schema_check( 2, "foo", { "foo" => "bar", "bar" => ["integer", {"range" => [-1,2]}] } )
-      lambda { schema_check( 3, "foo", { "foo" => "bar", "bar" => ["range", {"limit" => [-1,2] } ] } ) }.should raise_error
+      JsonShape.schema_check( 2, "foo", { "foo" => "bar", "bar" => ["integer", {"range" => [-1,2]}] } )
+      lambda { JsonShape.schema_check( 3, "foo", { "foo" => "bar", "bar" => ["range", {"limit" => [-1,2] } ] } ) }.should raise_error
     end
 
     it "should not allow undefined types" do
-      lambda { schema_check( 2, "bar", { "foo" => "integer" } ) }.should raise_error
+      lambda { JsonShape.schema_check( 2, "bar", { "foo" => "integer" } ) }.should raise_error
     end
   end
 end

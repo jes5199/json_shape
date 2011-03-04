3c058472061fc3b9d2489f986fda323cc96c515c
Jesse Wolfe <jes5199@gmail.com>
2011-03-03 17:26:17 -0800

    whitespace

--- schema_schema.js
+++ schema_schema.js
@@ -1,13 +1,13 @@
 {
-  "schema" : ["dictionary", {"contents": "definition"}],
-  "definition" : ["either", {"choices": ["definition_atom", "definition_singleton", "definition_pair"]}],
+  "schema" : [ "dictionary", {"contents": "definition"} ],
+  "definition" : [ "either", {"choices": ["definition_atom", "definition_singleton", "definition_pair"] } ],
 
-  "builtin_type" : ["enum", {
+  "builtin_type" : [ "enum", {
     "values": ["string", "number", "boolean", "null", "undefined", "array", "object", "anything",
                "literal", "optional", "integer", "enum", "range",
                "tuple", "dictionary", "either", "restrict"
               ]
-  }],
+  } ],
 
   "definition_atom" : [ "either", {
     "choices": [ "custom_type", "builtin_type_with_optional_parameters", "builtin_type_without_parameters" ]
@@ -34,24 +34,24 @@
       [ "tuple", {"elements": [ ["literal", "tuple"]     , "tuple_parameters"      ] } ],
       [ "tuple", {"elements": [ ["literal", "either"]    , "either_parameters"     ] } ]
     ]
-  }],
+  } ],
 
   "custom_type" : ["restrict", {
     "require": ["string"],
     "reject":  ["builtin_type"]
-  }],
+  } ],
 
   "builtin_type_without_parameters" : ["enum", {
     "values": ["string", "number", "boolean", "null", "undefined", "anything", "integer"]
-  }],
+  } ],
 
   "builtin_type_with_optional_parameters" :  ["enum", {
     "values": ["array", "object", "dictionary", "restrict"]
-  }],
+  } ],
 
   "builtin_type_with_mandatory_parameters" : ["enum", {
     "values": ["literal", "optional", "enum", "range", "tuple", "either"]
-  }],
+  } ],
 
   "optional_definition": [ "optional", "definition" ],
   "optional_definitions": [ "optional", ["array", {"contents": "definition"} ] ],
@@ -69,22 +69,22 @@
 
   "enum_parameters": [ "object", {"members": {
     "values" : ["array", {"contents": "anything"}]
-  } }],
+  } } ],
 
   "range_parameters": [ "object", {"members": {
     "limits" : ["tuple", {"elements": ["number", "number"]}]
-  }}],
+  } } ],
 
   "tuple_parameters": [ "object", {
     "members" : {
       "elements" : ["array", {"contents": "definition"}]
     }
-  }],
+  } ],
 
   "either_parameters": [ "object", {
     "members" : {
       "choices" : ["array", {"contents": "definition"}]
     }
-  }]
+  } ]
 
 }

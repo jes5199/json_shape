714bb14c07084d5450f759c5d357467d07fe2f14
Jesse Wolfe <jes5199@gmail.com>
2011-11-03 09:59:28 -0700

    whitespace

--- json_shape.rb
+++ json_shape.rb
@@ -180,19 +180,18 @@ end
 if __FILE__ == $0
   require 'rubygems'
   require 'json'
-  require 'json_shape'
-  
+
   schema = JSON.parse( File.read( ARGV[0] ) )
-  
+
   type = ARGV[1]
-  
+
   if ARGV[2]
     stream = File.open(ARGV[2])
   else
     stream = STDIN
   end
-  
+
   data = JSON.parse( stream.read )
-  
+
   JsonShape.schema_check( data, type, schema )
 end
